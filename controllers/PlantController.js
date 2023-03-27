4b1f38839e93c8367d28bb7ada5de94b0d9591b5
Bondarni <118389811+Bondarni@users.noreply.github.com>
2023-03-27 15:32:38 -0400

    created controllers and routes for plants

--- controllers/PlantController.js
+++ controllers/PlantController.js
@@ -1,7 +1,9 @@
-const { User, Plant, Environment } = require('../models')
+const { Plant } = require('../models')
 
 const GetPlants = async (req, res) => {
   try {
+    const plants = await Plant.findAll()
+    res.send(plants)
   } catch (error) {
     throw error
   }
@@ -9,6 +11,13 @@ const GetPlants = async (req, res) => {
 
 const GetPlantById = async (req, res) => {
   try {
+    let plantId = parseInt(req.params.plant_id)
+    const plant = await Plant.findAll({
+      where: {
+        id: plantId
+      }
+    })
+    res.send(plant)
   } catch (error) {
     throw error
   }
@@ -16,6 +25,14 @@ const GetPlantById = async (req, res) => {
 
 const EditPlant = async (req, res) => {
   try {
+    let plantId = parseInt(req.params.plant_id)
+    let updatedPlant = await Plant.update(req.body, {
+      where: {
+        id: plantId
+      },
+      returning: true
+    })
+    res.send(updatedPlant)
   } catch (error) {
     throw error
   }
@@ -23,6 +40,23 @@ const EditPlant = async (req, res) => {
 
 const DeletePlant = async (req, res) => {
   try {
+    let plantId = parseInt(req.params.plant_id)
+    await Plant.destroy({
+      where: {
+        id: plantId
+      },
+      returning: true
+    })
+    res.send({ message: 'Plant erased' })
+  } catch (error) {
+    throw error
+  }
+}
+
+const AddPlant = async (req, res) => {
+  try {
+    let newPlant = await Plant.create(req.body)
+    res.send(newPlant)
   } catch (error) {
     throw error
   }
@@ -32,5 +66,6 @@ module.exports = {
   GetPlants,
   GetPlantById,
   EditPlant,
-  DeletePlant
+  DeletePlant,
+  AddPlant
 }
--- routes/PlantRouter.js
+++ routes/PlantRouter.js
@@ -1,7 +1,10 @@
 const Router = require('express').Router()
 const controller = require('../controllers/PlantController')
 
-// Router.get('/', controller.GetPlants)
-// Router.get('/:plant_id', controller.GetPlantById)
+Router.get('/', controller.GetPlants)
+Router.get('/:plant_id', controller.GetPlantById)
+Router.post('/', controller.AddPlant)
+Router.put('/:plant_id', controller.EditPlant)
+Router.delete('/:plant_id', controller.DeletePlant)
 
 module.exports = Router
