7d135dafbcc5df12cfbd2f092773eb13e659acae
Bondarni <118389811+Bondarni@users.noreply.github.com>
2023-03-30 12:16:14 -0400

    add area form now avaiable

--- client/src/pages/AddAreaPage/AddAreaPage.jsx
+++ client/src/pages/AddAreaPage/AddAreaPage.jsx
@@ -1,16 +1,44 @@
 import './AddAreaPage.css'
-import { Link } from 'react-router-dom'
+import axios from 'axios'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { BASE_URL } from '../../globals'
 const AddAreaPage = () => {
+  let navigate = useNavigate()
+
+  let initialState = {
+    name: '',
+    userId: 1
+  }
+
+  const [formState, setFormState] = useState(initialState)
+
+  const handleChange = async (e) => {
+    setFormState({ ...formState, [e.target.id]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    await axios.post(`${BASE_URL}/areas`, formState)
+    console.log(formState)
+    setFormState(initialState)
+    navigate('/areas')
+  }
+
   return (
     <div className="addareasection">
       <h1>New Space</h1>
-      <form action="">
-        <input type="text" />
-        <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <label for="name">Which room are you adding?</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          onChange={handleChange}
+          value={formState.name}
+        />
       </form>
-      <Link to={'/areas'}>
-        <button>Done!</button>
-      </Link>
+      <button>Done!</button>
     </div>
   )
 }
--- client/src/pages/AddPlantPage/AddPlantPage.jsx
+++ client/src/pages/AddPlantPage/AddPlantPage.jsx
@@ -1,7 +1,7 @@
 import './AddPlantPage.css'
 import axios from 'axios'
 import { useState, useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { BASE_URL } from '../../globals'
 const AddPlantPage = () => {
   let navigate = useNavigate()
