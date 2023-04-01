ac964b175699bd5aee28cb08bcde8bc78e686870
Bondarni <118389811+Bondarni@users.noreply.github.com>
2023-04-01 15:19:59 -0400

    installed auth dependencies, continued work on login page

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -3,12 +3,36 @@ import { NavLink } from 'react-router-dom'
 const NavBar = () => {
   return (
     <div className="navsection">
-      <NavLink to="/home">Home</NavLink>
-      <NavLink to="/about">About</NavLink>
-      <NavLink to="/areas">Your Spaces</NavLink>
-      <NavLink to="/plants">Your Plants</NavLink>
-      <NavLink to="/userinfo">Your Info</NavLink>
-      <NavLink to="/">Logout</NavLink>
+      <NavLink to="/home">
+        {({ isActive, isPending }) => (
+          <span className={isActive ? 'active' : ''}>Home</span>
+        )}
+      </NavLink>
+      <NavLink to="/about">
+        {({ isActive, isPending }) => (
+          <span className={isActive ? 'active' : ''}>About</span>
+        )}
+      </NavLink>
+      <NavLink to="/areas">
+        {({ isActive, isPending }) => (
+          <span className={isActive ? 'active' : ''}>Your Spaces</span>
+        )}
+      </NavLink>
+      <NavLink to="/plants">
+        {({ isActive, isPending }) => (
+          <span className={isActive ? 'active' : ''}>Your Plants</span>
+        )}
+      </NavLink>
+      <NavLink to="/userinfo">
+        {({ isActive, isPending }) => (
+          <span className={isActive ? 'active' : ''}>Your Info</span>
+        )}
+      </NavLink>
+      <NavLink to="/">
+        {({ isActive, isPending }) => (
+          <span className={isActive ? 'active' : ''}>Logout</span>
+        )}
+      </NavLink>
     </div>
   )
 }
